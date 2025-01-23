40daa459793d5263b74a86032302bece5d44dcad
kandarpa02 <kandarpaexe@gmail.com>
2025-01-23 20:42:05 +0530

    error fixed

--- src/test_model.py
+++ src/test_model.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.utils import accuracy
 
 
-data = pd.read_csv('/home/kandarpa-sarkar/Desktop/NeuraLite/data/train_test/mnist_test.csv')
+data = pd.read_csv('data/MNIST_data.csv')
 
 X = data.drop(data.columns[0], axis=1)
 y = data[data.columns[0]]
@@ -15,7 +15,7 @@ classes = 10
 y = np.eye(classes)[y] # encoding data
 
 # Accessing 'w' and 'b' from the loaded file
-w = np.load('/home/kandarpa-sarkar/Desktop/NeuraLite/model/weights.npy')
-b = np.load('/home/kandarpa-sarkar/Desktop/NeuraLite/model/biases.npy')
+w = np.load('model/weights.npy')
+b = np.load('model/biases.npy')
 
 print(accuracy(X, y, w, b))
--- src/train_model.py
+++ src/train_model.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.utils import gradient_descent
 
 
-data = pd.read_csv("/home/kandarpa-sarkar/Desktop/NeuraLite/data/MNIST_data.csv")
+data = pd.read_csv('data/MNIST_data.csv')
 
 X = data.drop(data.columns[0], axis=1)
 y = data[data.columns[0]]
@@ -23,8 +23,8 @@ y = np.eye(classes)[y]
 w, b = gradient_descent(X, y, 200, learning_rate=0.01, batch_size=512, tolerance=1e-4, patience=5)
 
 # Save the weights and biases
-np.save("/home/kandarpa-sarkar/Desktop/NeuraLite/model/weights.npy", w)
-np.save("/home/kandarpa-sarkar/Desktop/NeuraLite/model/biases.npy", b)
+np.save('model/weights.npy', w)
+np.save('model/weights.npy', b)
 
 
 model = {
@@ -33,7 +33,7 @@ model = {
 }
 
 # Save the model object to a .pkl file
-with open('/home/kandarpa-sarkar/Desktop/NeuraLite/model/trained_model.pkl', 'wb') as model_file:
+with open('model/trained_model.pkl', 'wb') as model_file:
     pickle.dump(model, model_file)
 
 print("Model saved as trained_model.pkl!")
